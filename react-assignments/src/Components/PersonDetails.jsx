35265059cf53baa4182b03c1524c3a01c4170672
Mandylaaane <ljungamanda@gmail.com>
2025-01-16 15:43:57 +0100

    Assignment 8: done

---
+++ react-assignments/src/Components/PersonDetails.jsx
@@ -0,0 +1,16 @@
+
+function PersonDetails ({person}) {
+
+    return (
+        <div>
+            <br/>
+            <h2>Name: {person.name}</h2>
+            <h2>Age: {person.info.age}</h2>
+            <h2>City: {person.info.city}</h2>
+            <h2>Job: {person.info.job}</h2>
+            <h2>Hobbies: {person.info.hobbies.join(', ')}</h2>
+        </div>
+    )
+}
+
+export default PersonDetails;
